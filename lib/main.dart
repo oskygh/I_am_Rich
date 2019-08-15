c1a80e10b96be95c5e60b69a0754d0d735e7fdbf
Iris <osbornem406@gmail.com>
2019-08-15 22:33:33 +0000

    Added image as asset

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,7 @@ void main() {
         ),
         body: Center(
           child:Image(
-              image: NetworkImage('https://image.shutterstock.com/image-photo/white-transparent-leaf-on-mirror-260nw-1029171697.jpg')
-          ),
+              image: AssetImage('images/3-2-diamond-png-image.png')
         ),
 
       ),
